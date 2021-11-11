d0d56fa78d04ae5fc84d0037f0f2c3e87b681f69
psyoon77 <42738772+psyoon77@users.noreply.github.com>
2021-11-11 07:35:45 -0500

    1. Graddle Adjustment

--- app/src/main/java/com/munsellapp/munsellcolorrecognitionapp/Calibrate.java
+++ app/src/main/java/com/munsellapp/munsellcolorrecognitionapp/Calibrate.java
@@ -8,7 +8,6 @@ import android.graphics.Color;
 import android.graphics.drawable.BitmapDrawable;
 import android.net.Uri;
 import android.provider.MediaStore;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
--- app/src/main/java/com/munsellapp/munsellcolorrecognitionapp/CalibrateHome.java
+++ app/src/main/java/com/munsellapp/munsellcolorrecognitionapp/CalibrateHome.java
@@ -8,7 +8,6 @@ import android.graphics.Color;
 import android.graphics.drawable.BitmapDrawable;
 import android.net.Uri;
 import android.provider.MediaStore;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -31,6 +30,8 @@ import static com.munsellapp.munsellcolorrecognitionapp.R.id.spinner5;
 
 import static com.munsellapp.munsellcolorrecognitionapp.R.id.spinner4;
 import static com.munsellapp.munsellcolorrecognitionapp.R.id.spinner6;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 
 public class CalibrateHome  extends AppCompatActivity implements View.OnClickListener{
